18fa6ecb0d3217958eced6d0e08792f3182873dd
Matt Murbach <mmurbach@uw.edu>
2019-10-15 13:55:16 -0700

    add SQLalchemy

--- frontend/example/server/__init__.py
+++ frontend/example/server/__init__.py
@@ -1,8 +1,11 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
 
 application = Flask(__name__,
                     static_folder="../static/dist",
                     template_folder="../static")
+application.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////test.db'
+db = SQLAlchemy(application)
 
 
 @application.route("/")

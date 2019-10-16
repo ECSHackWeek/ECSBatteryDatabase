551c5478f35f9402d6f4e64ab9d46eb8b292ea90
Dave Beck <dacb@uw.edu>
2019-10-16 08:44:04 -0400

    fixed up name

--- ecsbatdbm/version.py
+++ ecsbatdbm/version.py
@@ -30,7 +30,7 @@ description = "ecsbatdbm: a relational data model, API and toolchain for battery
 long_description = """
 ECSBatDBM
 =========
-Electrochemical Society Battery Database Manager (ecsbatdbm) is a relational
+Electrochemical Society Battery Database Model (ecsbatdbm) is a relational
 data model, reference implementation of the model and associated API and
 data management toolchain for battery related experimental data.  The
 prototype implementation was first developed at ECS Data Science Sprint at
